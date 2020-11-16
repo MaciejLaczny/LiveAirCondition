0e61a9d76d95762517534f39fb092c83f386c115
maciejlaczny <maciek.saci@gmail.com>
2020-11-16 17:31:40 +0100

    async gets updated

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-const api_values_url = 'http://api.gios.gov.pl/pjp-api/rest/data/getData/';
-const api_sensor_url = 'http://api.gios.gov.pl/pjp-api/rest/station/sensors/';
-const currentStationId = '501';
-const latestTest = 0;
-const PM10_id = 3;
-const PM25_id = 69;
+const api_values_url = 'http://api.gios.gov.pl/pjp-api/rest/data/getData/'
+const api_sensors_url = 'http://api.gios.gov.pl/pjp-api/rest/station/sensors/'
+const api_city_url = 'http://api.gios.gov.pl/pjp-api/rest/station/findAll'
+const stationName = 'Płock-Reja'
+const latestTest = 0
 
-const getSensorId = async (id, paramId) => {
-    const response = await fetch(api_sensor_url + id);
-    const data = await response.json();
-
-    const sensorId = data.find(item => item.param.idParam === paramId).id.toString();
-    return sensorId;
+const getCityID = async (stationName) => {
+    const response = await fetch(api_city_url)
+    const data = await response.json()
+    return data.find(city => city.stationName === stationName).id.toString()
 }
 
-const getSensorValues = async (pollutionId) => {
-    const sensorId = await getSensorId(currentStationId, pollutionId);
-
-    const response = await fetch(api_values_url + sensorId);
-    const data = await response.json();
-
-    const latestValue = data.values[latestTest].value;
-    return latestValue;
+const getCitySensors = async () => {
+    const cityID = await getCityID(stationName)
+    const response = await fetch(api_sensors_url + cityID)
+    const data = await response.json()
+    console.log(data)
+    return data
 }
 
-const setSensorValues = async () => {
-    let PM10_value = await getSensorValues(PM10_id);
-    //document.getElementById('PM10').textContent = PM10_value;
-
-    let PM25_value = await getSensorValues(PM25_id);
-    //document.getElementById('PM2.5').textContent = PM25_value;
+const getParamID = async (paramCode) => {
+    const citySensors = await getCitySensors()
+    const paramID = citySensors.find(sensor => sensor.param.paramCode === paramCode).id.toString()
+    return paramID
 }
 
-setSensorValues();
+const getParamValue = async (paramLabel) => {
+    const paramID = await getParamID(paramLabel)
+    const response = await fetch(api_values_url + paramID)
+    const data = await response.json()
+    return data.values[latestTest].value
+}
+
+const setSensor = async () => {
+    const PM10Label = document.getElementById('PM10Label').innerHTML
+    const PM10Value = await getParamValue(PM10Label)
+    document.getElementById('PM10Value').textContent = PM10Value
+}
+
+setSensor()
 
 
 
-//console.log(PM10_value);
+
+
+
+
+
+
+
